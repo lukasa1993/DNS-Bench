31f28708ed34f79c43ed87b9f88294bdeba22028
luka dodelia <luka@picktek.com>
2019-05-02 10:38:01 +0400

    Initial Commit

---
+++ DNS Bench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DNS Bench
+//
+//  Created by LD on 5/2/19.
+//  Copyright © 2019 LD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
